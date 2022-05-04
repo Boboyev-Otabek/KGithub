1d6976b8198a46f716e972e67e2f6beda85ae596
Otabek <boboyevotabek551@gmail.com>
2022-05-04 23:55:41 +0500

    Initial commit

---
+++ app/src/main/java/com/example/kgithub/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.kgithub
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initView()
+    }
+    fun initView(){
+        val open_second=findViewById<Button>(R.id.open_second)
+        open_second.setOnClickListener{
+            DetailActivity()
+        }
+    }
+    fun DetailActivity(){
+       var intent= Intent(this,SecondActivity::class.java)
+        startActivity(intent)
+    }
+}
